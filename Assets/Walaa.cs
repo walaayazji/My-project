8f1f354ef0f187ec2b2f4a1007257cf2074bd6dd
Walaa Alyazji <walaayazji@gmail.com>
2024-10-29 13:23:35 +0200

    first commit

---
+++ Assets/Walaa.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walaa : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int x=10;
+        string y="Yazji";
+
+        Debug.Log(x + 5);
+        Debug.Log(y);
+        Debug.LogWarning("uyarı");
+        Debug.LogError("Hata");
+    }}
+
